c12931bd44054a2f49bdd81e39d908e8053eee2c
xuhangc <cxhhg11@gmail.com>
2025-03-03 09:54:13 +0800

    test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from data import get_validation_data
+from data import get_validation_data, get_training_data
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -32,14 +32,14 @@ def test():
     val_file1 = opt.TRAINING.VAL_FILE1
     val_file2 = opt.TRAINING.VAL_FILE2
 
-    val_dataset1 = get_validation_data(val_file1, None)
+    val_dataset1 = get_training_data(val_file1, None)
     val_dataset2 = get_validation_data(val_file2, None)
 
     testloader1 = DataLoader(dataset=val_dataset1, batch_size=1, shuffle=False, num_workers=16, drop_last=False, pin_memory=True)
     testloader2 = DataLoader(dataset=val_dataset2, batch_size=1, shuffle=False, num_workers=16, drop_last=False, pin_memory=True)
 
     # Model & Metrics
-    model = DeShadowNet()
+    model = LG_ShadowNet()
 
     load_checkpoint(model, opt.TESTING.WEIGHT)
 
@@ -55,7 +55,7 @@ def test():
     stat_lpips = 0
     stat_rmse = 0
 
-    for _, test_data in enumerate(tqdm(testloader1)):
+    for idx, test_data in enumerate(tqdm(testloader1)):
         # get the inputs; data is a list of [targets, inputs, filename]
         inp = test_data[0].contiguous()
         tar = test_data[1]
@@ -64,7 +64,7 @@ def test():
         with torch.no_grad():
             res = model(inp, mas).clamp(0, 1)
 
-        save_image(res, os.path.join(opt.MODEL.SESSION, 'UCB', test_data[2][0]))
+        save_image(res, os.path.join(opt.MODEL.SESSION, 'ASFW', str(idx) + '.png'))
 
         stat_psnr += peak_signal_noise_ratio(res, tar, data_range=1).item()
         stat_ssim += structural_similarity_index_measure(res, tar, data_range=1).item()
@@ -83,7 +83,7 @@ def test():
     stat_lpips = 0
     stat_rmse = 0
 
-    for _, test_data in enumerate(tqdm(testloader2)):
+    for idx, test_data in enumerate(tqdm(testloader2)):
         # get the inputs; data is a list of [targets, inputs, filename]
         inp = test_data[0].contiguous()
         tar = test_data[1]
@@ -92,7 +92,7 @@ def test():
         with torch.no_grad():
             res = model(inp, mas).clamp(0, 1)
 
-        save_image(res, os.path.join(opt.MODEL.SESSION, 'ASFW', test_data[2][0]))
+        save_image(res, os.path.join(opt.MODEL.SESSION, 'UCB', str(idx) + '.png'))
 
         stat_psnr += peak_signal_noise_ratio(res, tar, data_range=1).item()
         stat_ssim += structural_similarity_index_measure(res, tar, data_range=1).item()
